c23501530cefb581a5b3c2831531037b96efbe09
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-08-23 15:05:40 +0600

    radiusToArea-fixed

--- RadiusToArea.js
+++ RadiusToArea.js
@@ -1,43 +1,19 @@
 var PI = 3.14;
-function area(radius){
-    return PI*radius*radius;
-}
-
-function main (input){
-    const radius = parseFloat(input.trim());
-    console.log("The area of the circle is " + parseFloat(area(radius)) + " square units.");
-}
-
-
-process.stdin.on('data', data => {
-    main(data.toString());
-})
-
-
-/**
- * ! Accepted by CodeMama
- * // Updated PI value with reduced precision
-var PI = 3.14;
 
 function area(radius) {
     return PI * radius * radius;
 }
 
-function roundToTwoDecimalPlaces(value) {
-    return Math.round(value * 100) / 100;
+function formatToTwoDecimalPlaces(value) {
+    return value.toFixed(2);
 }
 
 function main(input) {
     const radius = parseFloat(input.trim());
     let calculatedArea = area(radius);
-    
-    // Check if the area is 26002.34 and update it to 26002.3
-    if (calculatedArea === 26002.34) {
-        calculatedArea = 26002.3;
-    }
 
-    // Round the calculatedArea to two decimal places
-    calculatedArea = roundToTwoDecimalPlaces(calculatedArea);
+    // Format the calculatedArea to two decimal places
+    calculatedArea = formatToTwoDecimalPlaces(calculatedArea);
 
     console.log("The area of the circle is " + calculatedArea + " square units.");
 }
@@ -45,5 +21,3 @@ function main(input) {
 process.stdin.on('data', data => {
     main(data.toString());
 });
-
- */
